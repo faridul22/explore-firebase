b29a475441e81085ee35f55e4badbbbbd4b9e01b
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-12-11 23:12:08 +0600

    signInWithGoogle complete

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,8 +1,21 @@
-
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from './../../../firebase/firebase.init';
 
 const Login = () => {
+    const auth = getAuth(app)
+    const provider = new GoogleAuthProvider();
     const handleGoogleSingIn = () => {
         console.log('google mama is coming')
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.message;
+                console.log(errorCode)
+            });
     }
     return (
         <div className=''>
