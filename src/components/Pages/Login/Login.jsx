549ec8a3999e2f8edc51f14421b9d68b907f4267
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-12-14 09:45:08 +0600

    Github authentication added

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,14 +1,19 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from './../../../firebase/firebase.init';
+import { useState } from "react";
 
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
     const handleGoogleSingIn = () => {
         console.log('google mama is coming')
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
             .then((result) => {
                 const user = result.user;
+                setUser(user)
                 console.log(user)
             })
             .catch((error) => {
@@ -17,9 +22,33 @@ const Login = () => {
                 console.log(errorCode)
             });
     }
+    const handleGithubSingIn = () => {
+        signInWithPopup(auth, githubProvider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+                setUser(user)
+            }).catch((error) => {
+                console.log(error)
+            });
+    }
+    const handleLogOut = () => {
+
+    }
     return (
         <div className=''>
-            <button className="bg-green-700 px-10 py-2 text-white rounded-lg" onClick={handleGoogleSingIn}>Login</button>
+            {user ? <button className="bg-green-700 px-10 py-2 text-white rounded-lg" onClick={handleLogOut}>Log Out</button> :
+                <>
+                    <button className="bg-green-700 px-10 py-2 text-white rounded-lg" onClick={handleGoogleSingIn}>Google Login</button>
+                    <button className="bg-green-700 px-10 py-2 text-white rounded-lg" onClick={handleGithubSingIn}>Github Login</button>
+                </>
+            }
+
+            {user && <div className="">
+                <h1>User: {user?.displayName} </h1>
+                <h2>Email: {user?.email}</h2>
+                <img className="mx-auto" src={user.photoURL} alt="" />
+            </div>}
         </div>
     );
 };
